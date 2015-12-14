0ab85fa92192ece89ca50be80698ff1688ba8653
Nick Hendren <nickhendren@Nicks-MacBook-Pro-2.local>
2015-12-14 14:18:04 -0800

    finished up the routes/api.js file

--- app/routes/api.js
+++ app/routes/api.js
@@ -125,4 +125,101 @@ module.exports = function(app, express) {
     }
   });
 
-}
+  //test route to make sure everything is working
+  //accessed at GET http://localhost:3000/api
+  apiRouter.get('/', function(req, res){
+    res.json({message: 'Horray! Welcome to our API!'});
+  });
+
+  //on routes that end in /users
+  apiRouter.route('users')
+
+    //create a user (accessed at POST http://localhost:3000/users)
+    .post(function(req, res){
+
+      var user = new User();                    //create a new instance of the User model
+      user.first_name = req.body.first_name;    //set the users first name(comes from the request)
+      user.last_name = req.body.last_name;      //set the users last name(comes from the request)
+      user.username = req.body.username;        //set the users username(comes from the request)
+      user.email = req.body.email               //set the users email(comes from the request)
+      user.password = req.body.password;        //set the users password(comes from the request)
+
+      user.save(function(err){
+        if (err) {
+          //duplicate entry
+          if (err.code == 11000)
+            return res.json({success: false, message: 'A user with that username already exists.'});
+          else
+            return res.send(err);
+        }
+
+        //return a message
+        res.json({message: 'User created!'});
+      });
+    })
+
+    //get all the users(acessed at GET http://localhost:3000/api/users)
+    .get(function(req, res){
+
+      User.find({}, function(err, users){
+        if (err) res.send(err);
+
+        //return the users
+        res.json(users)
+      });
+    });
+
+  //on routes that end in /users/:user_id
+  api.Router.route('/users/:user_id')
+
+    //get the user with that id
+    .get(function(req, res){
+      User.findById(req.params.user_id, function(err, user){
+        if (err) res.send(err);
+
+        //return that user
+        res.json(user);
+      });
+    })
+
+  //update the user with this id
+  .put(function(req, res){
+    User.findById(req.params.user_id, function(err, user){
+
+      if (err) res.send(err);
+
+      //set the new user information if it exists in the request
+      if (req.body.first_name) user.first_name = req.body.first_name;
+      if (req.body.last_name) user.last_name = req.body.last_name;
+      if (req.body.username) user.username = req.body.username;
+      if (req.body.email) user.email = req.body.email;
+      if (req.body.password) user.password = req.body.password;
+
+      //save the user
+      user.save(function(err){
+        if (err) res.send(err);
+
+        //retuen a message
+        res.json({message: 'User updated!'});
+      });
+    });
+  })
+
+  //delete the user with this id
+  .delete(function(req, res){
+    User.remove({
+      _id: req.params.user_id
+    }, function(err, user){
+      if (err) res.send(err);
+
+      res.json({message: 'Successfully deleted'});
+    });
+  });
+
+  //api endpoint to get user information
+  apiRouter.get('/me', function(req, res){
+    res.send(req.decoded);
+  });
+
+  return apiRouter
+};
