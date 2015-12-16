3f9ca9a950bb6a80713ce537c8b15aae0198f4a5
Sean Esteva <seangeleno@gmail.com>
2015-12-16 09:25:18 -0800

    added bootstrap links and links to controllers in landing_page

--- app/routes/api.js
+++ app/routes/api.js
@@ -89,43 +89,43 @@ module.exports = function(app, express) {
 
     });
   });
-
-  //route middleware to verify a token
-  apiRouter.use(function(req, res, next){
-    //do logging
-    console.log('Somebody just came to our app!');
-
-    //check header or url parameters or post parameters for token
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-    //decode token
-    if (token) {
-      //verifies secret and checks express
-      jwt.verify(token, superSecret, function(err, decoded){
-
-        if (err) {
-          res.status(403).send({
-            success: false,
-            message: 'Failed to authenticate token.'
-          });
-        } else {
-          //if everything is good, save to request for use in other routes
-          req.decoded = decoded;
-
-          next();
-        }
-      });
-
-    } else {
-      //if there is no token return HTTP response of 403 (access forbidden)
-      res.status(403).send({
-        success: false,
-        message: 'No token provided.'
-      });
-
-
-    }
-  });
+  //
+  // //route middleware to verify a token
+  // apiRouter.use(function(req, res, next){
+  //   //do logging
+  //   console.log('Somebody just came to our app!');
+  //
+  //   //check header or url parameters or post parameters for token
+  //   var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  //
+  //   //decode token
+  //   if (token) {
+  //     //verifies secret and checks express
+  //     jwt.verify(token, superSecret, function(err, decoded){
+  //
+  //       if (err) {
+  //         res.status(403).send({
+  //           success: false,
+  //           message: 'Failed to authenticate token.'
+  //         });
+  //       } else {
+  //         //if everything is good, save to request for use in other routes
+  //         req.decoded = decoded;
+  //
+  //         next();
+  //       }
+  //     });
+  //
+  //   } else {
+  //     //if there is no token return HTTP response of 403 (access forbidden)
+  //     res.status(403).send({
+  //       success: false,
+  //       message: 'No token provided.'
+  //     });
+  //
+  //
+  //   }
+  // });
 
   //test route to make sure everything is working
   //accessed at GET http://localhost:3000/api
